391b9ea626aa3b4edac633bb956ea05302e90caf
Himesh <himesh@pixelcrayons.com>
2015-07-31 17:15:13 +0530

    1. Added inspector selection criteria in inspection request form and validation for the same.

--- app/models/insp_request.rb
+++ app/models/insp_request.rb
@@ -6,7 +6,9 @@ class InspRequest < ActiveRecord::Base
 
   REFERRAL_SOURCE = %w(Internet Advertisement TV Friend Other)
 
-  validates :callTime, :client_id, :property_id, presence: true
+  SELECTION_CRITERIA = ['Specific Inspector', 'Senior Inspector', 'Next Available']
+
+  validates :callTime, :client_id, :property_id, :selectInsp, presence: true
   validates :client_id, uniqueness: { scope: :property_id, message: "Property has already been assigned for same customer." }
 
   def has_appointment?

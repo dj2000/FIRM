9e7fef129c2926609fb4e692c7c90ad1a252e79e
Himesh <himesh@pixelcrayons.com>
2015-08-13 16:14:06 +0530

    Setting default year as 1965

--- app/models/property.rb
+++ app/models/property.rb
@@ -11,7 +11,7 @@ class Property < ActiveRecord::Base
 
   PROPERTY_TYPE = %w(SFR MFR)
 
-  YEAR_BUILT = (1965..Date.current.year)
+  YEAR_BUILT = (1901..1965)
 
   validates :street, :city, :state, presence: true
 
@@ -21,13 +21,25 @@ class Property < ActiveRecord::Base
                 { is: 5,
                   allow_blank: true }
 
+  before_save :set_default_year_built
 
+  #To populate select dropdown
   def property_select_value
     state_name = CS.states(:us)[ self.try(:state).try(:to_sym) ]
     "#{number} #{street}, #{city}, #{state_name} #{zip}"
   end
-  
+
+  #For getting full name of state
   def state_name
     state_name = CS.states(:us)[ self.try(:state).try(:to_sym) ]
+  end
+
+  def humanize(attribute)
+    self.send("#{attribute}") ? "Yes" : "No"
+  end
+
+  # For setting default year built
+  def set_default_year_built
+    self.yearBuilt = 1965 if self.yearBuilt.blank?
   end
 end

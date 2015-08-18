bf84064fce56e941236ac50c746ee554e6f99f25
Himesh <himesh@pixelcrayons.com>
2015-08-18 14:12:53 +0530

    deleted intializer file of wicked_pdf

--- config/initializers/wicked_pdf.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env.production?
-  wkhtmltopdf_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-else
-  wkhtmltopdf_path = "/home/webwerks/.rvm/gems/ruby-2.0.0-p643/bin/wkhtmltopdf"
-end
-
-WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }

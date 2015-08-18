d791de7c24cb2f910ffe0617f38892c9abf6cb80
Himesh <himesh@pixelcrayons.com>
2015-08-18 13:14:21 +0530

    Changing path of wkhtmltopdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  wkhtmltopdf_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+  wkhtmltopdf_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
 else
   wkhtmltopdf_path = "/home/webwerks/.rvm/gems/ruby-2.0.0-p643/bin/wkhtmltopdf"
 end

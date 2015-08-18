a4e71cba480911eedd1990321c370e48e594f3c8
Himesh <himesh@pixelcrayons.com>
2015-08-18 13:06:50 +0530

    resolved issue of wicked pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,7 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/home/webwerks/.rvm/gems/ruby-2.0.0-p643/bin/wkhtmltopdf'
-}
+if Rails.env.production?
+  wkhtmltopdf_path = Rails.root.to_s + "/bin/wkhtmltopdf"
+else
+  wkhtmltopdf_path = "/home/webwerks/.rvm/gems/ruby-2.0.0-p643/bin/wkhtmltopdf"
+end
+
+WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }

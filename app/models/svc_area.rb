3d73a04507ab4ad92b96a140fb20a835aa81c600
Himesh <himesh@pixelcrayons.com>
2015-08-19 13:31:03 +0530

    UI corrections for Service area

--- app/models/svc_area.rb
+++ app/models/svc_area.rb
@@ -1,3 +1,20 @@
 class SvcArea < ActiveRecord::Base
   has_many :appointments
+  validates :city, :state, presence: true
+
+  validates :zip,
+              presence: true,
+              length:
+                { is: 5,
+                  allow_blank: true }
+
+  #For getting full name of state
+  def state_name
+    state_name = CS.states(:us)[ self.try(:state).try(:to_sym) ]
+  end
+
+  def is_serviced
+    self.serviced? ? "Yes" : "No"
+  end
+
 end

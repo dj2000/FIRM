af832b6f257efbb42ea3e63cae4eeeec44d03855
Himesh <himesh@pixelcrayons.com>
2015-09-02 13:37:37 +0530

    UI corrections and validations for Payment Plan

--- app/models/pay_plan.rb
+++ app/models/pay_plan.rb
@@ -1,3 +1,14 @@
 class PayPlan < ActiveRecord::Base
   belongs_to :bid
+
+  validates :pmt1Pcnt, :pmt2Pcnt, :pmt3Pcnt, :jobMinAmt, :jobMaxAmt, presence: true
+
+  def deposit
+		total = 0
+		[:pmt1Pcnt, :pmt2Pcnt, :pmt3Pcnt, :pmt4Pcnt, :pmt5Pcnt].each do |col|
+			total += self.send(col) unless self.send(col).blank?
+		end
+		total
+  end
+
 end

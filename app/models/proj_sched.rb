4c11928687798173ee7e7b3ccb4bf385bfba3793
Himesh <himesh@pixelcrayons.com>
2015-09-14 18:34:41 +0530

    Added validation for crew schedule

--- app/models/proj_sched.rb
+++ app/models/proj_sched.rb
@@ -4,6 +4,8 @@ class ProjSched < ActiveRecord::Base
 
   validates :crew_id, :schedule_start_date, :schedule_end_date, :startTime, :endTime, presence: true
 
+  validate :availability_of_crew
+
   COLORS = Crew.assign_colors
 
   def as_json
@@ -23,5 +25,15 @@ class ProjSched < ActiveRecord::Base
 
   def sched_end
 		"#{schedule_end_date.strftime("%Y-%m-%d")} #{endTime.strftime("%I:%M %p")}".to_datetime
+  end
+
+  private
+
+  def availability_of_crew
+		double_book = self.crew_id and self.try(:crew).try(:double_book?)
+		start_time = self.startTime.strftime("%H:%M:%S")
+		end_time = self.endTime.strftime("%H:%M:%S")
+		crews_availability = ProjSched.where('project_id != ? AND crew_id = ? AND (((schedule_start_date BETWEEN ? AND ?) AND ("startTime" BETWEEN ? AND ?)) OR ((schedule_end_date BETWEEN ? AND ?) AND ("endTime" BETWEEN ? AND ?)))', self.project_id, self.crew_id, self.schedule_start_date, self.schedule_end_date, start_time, end_time, self.schedule_start_date, self.schedule_end_date, start_time, end_time)
+		self.errors.add(:crew_id, "Crew is not available for this time slot.") if double_book == false || crews_availability.present?
   end
 end

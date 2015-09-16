3d4a597cd053eaa0c51c26454320928d30f96247
Himesh <himesh@pixelcrayons.com>
2015-09-16 14:39:08 +0530

    Resolved issue of condition for project schedule

--- app/models/proj_sched.rb
+++ app/models/proj_sched.rb
@@ -31,10 +31,12 @@ class ProjSched < ActiveRecord::Base
   private
 
   def availability_of_crew
-		double_book = self.crew_id and self.try(:crew).try(:double_book?)
+		double_book = (self.crew_id and self.try(:crew).try(:double_book?))
 		start_time = self.startTime.strftime("%H:%M:%S")
 		end_time = self.endTime.strftime("%H:%M:%S")
-		crews_availability = ProjSched.where('project_id != ? AND crew_id = ? AND (((schedule_start_date BETWEEN ? AND ?) AND ("startTime" BETWEEN ? AND ?)) OR ((schedule_end_date BETWEEN ? AND ?) AND ("endTime" BETWEEN ? AND ?)))', self.project_id, self.crew_id, self.schedule_start_date, self.schedule_end_date, start_time, end_time, self.schedule_start_date, self.schedule_end_date, start_time, end_time)
+    if double_book
+      crews_availability = ProjSched.where('project_id != ? AND crew_id = ? AND (((schedule_start_date BETWEEN ? AND ?) AND ("startTime" BETWEEN ? AND ?)) OR ((schedule_end_date BETWEEN ? AND ?) AND ("endTime" BETWEEN ? AND ?)))', self.project_id, self.crew_id, self.schedule_start_date, self.schedule_end_date, start_time, end_time, self.schedule_start_date, self.schedule_end_date, start_time, end_time)
+    end
 		self.errors.add(:crew_id, "Crew is not available for this time slot.") if double_book == false || crews_availability.present?
   end
 end

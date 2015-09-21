e1cb4f43a42cacdfbf9d1f034e6d4301e042204c
Himesh <himesh@pixelcrayons.com>
2015-09-21 12:22:24 +0530

    1. Added total in bid's index page 2. Display alert if no inspection or project selected

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -3,12 +3,20 @@ class Bid < ActiveRecord::Base
   belongs_to :payPlan
   has_one :comm_history
 
-  validates :costRepair, :feeSeismicUpg, :feeAdmin, :inspection_id, :title, presence: true
+  validates :costRepair, :feeSeismicUpg, :feeAdmin, :inspection_id, :title, :payPlan_id, presence: true
 
   before_create :default_status
 
   # Set default status as pending
   def default_status
 		self.status = "Pending"
+  end
+
+  def total_cost
+		total = 0
+		[:costRepair, :feeSeismicUpg, :feeAdmin].each do |attribute|
+			total += self.send(attribute)
+		end
+		total
   end
 end

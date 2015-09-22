d055a5f3ab81d8e24a307850ebc3e4345c6c7df5
Himesh <himesh@pixelcrayons.com>
2015-09-22 14:50:16 +0530

    Added notes field in inspector

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921082753) do
+ActiveRecord::Schema.define(version: 20150922091712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -226,6 +226,7 @@ ActiveRecord::Schema.define(version: 20150921082753) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "notes"
   end
 
   create_table "invoices", force: true do |t|

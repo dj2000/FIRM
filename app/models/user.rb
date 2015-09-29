f9645eec6db83b30b46237f15ab3bd118b448ac7
Himesh <himesh@pixelcrayons.com>
2015-09-29 17:57:23 +0530

    1. Added first name and last name to users 2. Caller should be current_user for Bid follow up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, presence: true
+
+  def name
+  	"#{self.try(:first_name)} #{self.try(:last_name)}"
+  end
 end

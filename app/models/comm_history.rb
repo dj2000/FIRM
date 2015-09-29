09429db784fe467ce418c07380025d49f86908c8
Himesh <himesh@pixelcrayons.com>
2015-09-29 19:00:38 +0530

    added validation for Call Back date

--- app/models/comm_history.rb
+++ app/models/comm_history.rb
@@ -3,4 +3,10 @@ class CommHistory < ActiveRecord::Base
   belongs_to :bid
 
   validates :caller, :recipient, :callSummary, :notes, :bid_id, presence: true
+
+  validate :call_back_date, if: Proc.new{|c| c.callOutcome == "CallBack" }
+
+  def call_back_date
+		self.errors.add(:callBackDate, "Call Back Date should be greater than Call Date.") unless self.callBackDate >= self.call_time
+  end
 end

e3b687db0ce6edf25a1e8be0444c65463178b190
Himesh <himesh@pixelcrayons.com>
2015-10-01 16:36:23 +0530

    Calculation for invoice amount

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -5,8 +5,17 @@ class Receipt < ActiveRecord::Base
   after_save :update_invoice_balance
 
   def update_invoice_balance
-		invoice = self.try(:invoice)
-		invoice_amount = invoice.try(:amount).try(:to_f) - self.try(:amount).try(:to_f)
-		invoice.update_attributes(amount: invoice_amount)
+    amount_values = self.amount_change.map(&:to_f)
+    new_amount = amount_values.last
+    prev_amount = amount_values.first
+    if new_amount > prev_amount
+      difference_amount = new_amount - prev_amount
+      invoice_amount = invoice.try(:amount).try(:to_f) - difference_amount.to_f
+    elsif new_amount < prev_amount
+      difference_amount = prev_amount - new_amount
+      invoice_amount = invoice.try(:amount).try(:to_f) + difference_amount.to_f
+    end
+    invoice = self.try(:invoice)
+    invoice.update_attributes(amount: invoice_amount)
   end
 end

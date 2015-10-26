7386fba5cc2e7f3bd9e67ca074d9c71273a7a366
Himesh <himesh@pixelcrayons.com>
2015-10-26 12:13:17 +0530

    changes for inspection entry form for file upload

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ActiveRecord::Base
 	belongs_to :attachable, polymorphic: true
 	has_attached_file :attachment
-	validates_attachment_content_type :attachment, content_type: %w( application/msword application/pdf application/vnd.openxmlformats-officedocument.wordprocessingml.document )
+	validates_attachment_content_type :attachment, content_type: %w( application/msword application/pdf application/vnd.openxmlformats-officedocument.wordprocessingml.document image/jpg image/png )
 end

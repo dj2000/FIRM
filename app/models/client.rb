c0f79ee08acce5ba8b69e64b33854e2b5f0b9e4f
Himesh <himesh@pixelcrayons.com>
2015-11-16 16:25:21 +0530

    removed unwanted code for associated customers

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,7 +4,6 @@ class Client < ActiveRecord::Base
   has_many :agent_clients, dependent: :destroy
   has_many :agents, through: :agent_clients, dependent: :destroy
   has_many :insp_requests, dependent: :destroy
-  has_many :associated_customers, class_name: "Client", foreign_key: "parent_id"
 
   validates :firstName, :lastName, presence: true, if: lambda { |o| o.of_type == "Individual" or o.of_type.blank? }
   validates :phoneH, :phoneW, :phoneC,
@@ -14,8 +13,6 @@ class Client < ActiveRecord::Base
   validates :email, email_format: { message: "Invalid Email Address" }
 
   validates :company_name, presence: true, if: lambda { |o| o.of_type == "Company" }
-
-  accepts_nested_attributes_for :associated_customers, allow_destroy: true
 
   CLIENT_TYPE = %w(Buyer Owner Seller Contractor Other)
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113114653) do
+ActiveRecord::Schema.define(version: 20151116091430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,7 +108,6 @@ ActiveRecord::Schema.define(version: 20151113114653) do
     t.string   "client_type"
     t.string   "company_name"
     t.string   "of_type"
-    t.integer  "parent_id"
   end
 
   create_table "comm_histories", force: true do |t|

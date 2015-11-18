fed2a9419ff777d0d23ce0b81a2d322cdb5f0e0f
Himesh <himesh@pixelcrayons.com>
2015-11-18 17:17:51 +0530

    added Receipt report

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -68,6 +68,18 @@ class ReceiptsController < ApplicationController
     end
   end
 
+  def report_result
+    start_date = DateTime.parse(params[:start_date])
+    end_date = DateTime.parse(params[:end_date])
+    @receipts = Receipt.where('("date" BETWEEN ? AND ?)',start_date, end_date)
+  end
+
+  def print
+    start_date = DateTime.parse(params[:start_date])
+    end_date = DateTime.parse(params[:end_date])
+    @receipts = Receipt.where('("date" BETWEEN ? AND ?)',start_date, end_date)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_receipt

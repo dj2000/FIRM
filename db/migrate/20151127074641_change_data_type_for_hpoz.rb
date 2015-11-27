9f3ac61375093a123b92bb3d4a3d56c9d45acf63
himesh <himesh@pixelcrayons.com>
2015-11-27 14:11:29 +0530

    Migrating existent database for HPOZ of property

--- db/migrate/20151127074641_change_data_type_for_hpoz.rb
+++ db/migrate/20151127074641_change_data_type_for_hpoz.rb
@@ -1,12 +1,14 @@
 class ChangeDataTypeForHpoz < ActiveRecord::Migration
   def change
-    @properties = Property.all
+    properties_hash = Hash.new
+    Property.all.each do |property|
+      properties_hash[property.id] = property.hpoz? ? "Yes" : "No"
+    end
     remove_column :properties, :hpoz, :boolean
     add_column :properties, :hpoz, :string
     Property.reset_column_information
-    @properties.each do |property|
-      value = property.hpoz ? "Yes" : "No"
-      property.update(hpoz: value)
+    Property.all.each do |property|
+      property.update(hpoz: properties_hash[property.id])
     end
   end
 end

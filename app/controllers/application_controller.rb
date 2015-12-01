3719829497b82d6cd592c727c48d8d49a9e3f1f0
himesh <himesh@pixelcrayons.com>
2015-12-01 19:13:35 +0530

    Resolved issue of request referer of the role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,28 @@ class ApplicationController < ActionController::Base
     @cities = CS.cities(state.to_sym, :us)
     render json: @cities
   end
+
+  def the_role_default_access_denied_response
+    access_denied_msg = t(:access_denied, scope: :the_role)
+    if request.xhr?
+      render json: {
+        errors: { the_role: [ access_denied_msg ] },
+
+        controller_name:      controller_path,
+        action_name:          action_name,
+        has_access_to_action: current_user.try(:has_role?, controller_path, action_name),
+
+        current_user: { id: current_user.try(:id) },
+
+        owner_check_object: {
+          owner_check_object_id:    @owner_check_object.try(:id),
+          owner_check_object_class: @owner_check_object.try(:class).try(:to_s)
+        },
+
+        has_access_to_object: current_user.try(:owner?, @owner_check_object)
+      }, status: 401
+    else
+      redirect_to root_path, notice: { error: access_denied_msg }
+    end
+  end
 end

3a03c26c441f3549d3dcb867b9acb8ab615af490
Himesh <himesh@pixelcrayons.com>
2015-12-07 13:01:54 +0530

    Approved existing user in migration

--- db/migrate/20151204083903_add_approved_to_user.rb
+++ db/migrate/20151204083903_add_approved_to_user.rb
@@ -1,5 +1,7 @@
 class AddApprovedToUser < ActiveRecord::Migration
   def change
     add_column :users, :approved, :boolean, default: false
+    User.reset_column_information
+    User.update_all(approved: true)
   end
 end

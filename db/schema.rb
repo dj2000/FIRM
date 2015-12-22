11a55a75e0895ea2fc458ea8ee9b52507beb00bc
himesh <himesh@pixelcrayons.com>
2015-12-22 15:14:25 +0530

    added email in crew model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222061554) do
+ActiveRecord::Schema.define(version: 20151222094000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,6 +194,7 @@ ActiveRecord::Schema.define(version: 20151222061554) do
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
   create_table "documents", force: true do |t|

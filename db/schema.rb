52c0786dbba988fc97e322dce04add6469cee12f
himesh <himesh@pixelcrayons.com>
2015-12-22 19:46:05 +0530

    Add a drop-down for primary crew on project form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222094813) do
+ActiveRecord::Schema.define(version: 20151222135336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -400,6 +400,7 @@ ActiveRecord::Schema.define(version: 20151222094813) do
     t.date     "schedule_pref_start"
     t.date     "schedule_pref_end"
     t.boolean  "permit"
+    t.integer  "primary_crew_id"
   end
 
   create_table "properties", force: true do |t|

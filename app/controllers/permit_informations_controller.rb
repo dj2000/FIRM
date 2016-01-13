3bdc7650100382c9d4e7faa3c8cfb70e9ff77c13
himesh <himesh@pixelcrayons.com>
2016-01-13 19:54:19 +0530

    removing binding pry

--- app/controllers/permit_informations_controller.rb
+++ app/controllers/permit_informations_controller.rb
@@ -2,7 +2,6 @@ class PermitInformationsController < ApplicationController
 	def send_email
 		@draftsman = Draftsman.find(params[:draftsman_id])
 		@permit_information = PermitInformation.find(params[:permit_information_id])
-		binding.pry
 		UserMailer.send_email_for_permit(@draftsman, @permit_information, params[:cc_emails], params[:notes]).deliver
     respond_to do |format|
       format.html{ redirect_to root_path, notice: 'Email is sent successfully.' }

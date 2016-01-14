6126b164c18385fd7eaa8fd5c518ff0b1e1731b4
himesh <himesh@pixelcrayons.com>
2016-01-14 19:37:32 +0530

    added settings for config email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "",
+    :user_name            => "firm.inspection@gmail.com",
+    :password             => "firmi123",
+    :authentication       => "plain",
+    :enable_starttls_auto => true,
+    :openssl_verify_mode  => 'none'
+  }
 end

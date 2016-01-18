e80c1fe972676a4d5d1415afec60cced4c7fd540
himesh <himesh@pixelcrayons.com>
2016-01-18 13:01:57 +0530

    Add “Email File Upload” feature to the UI for bid's follow up

--- app/controllers/comm_histories_controller.rb
+++ app/controllers/comm_histories_controller.rb
@@ -48,6 +48,8 @@ class CommHistoriesController < ApplicationController
     @comm_history = CommHistory.new(comm_history_params)
     respond_to do |format|
       if @comm_history.save
+        @inspection = @comm_history.try(:bid).try(:inspection)
+        create_documents
         format.html { redirect_to @comm_history, notice: 'Bids follow up was successfully created.' }
         format.json { render :show, status: :created, location: @comm_history }
       else
@@ -62,6 +64,8 @@ class CommHistoriesController < ApplicationController
   def update
     respond_to do |format|
       if @comm_history.update(comm_history_params)
+        @inspection = @comm_history.try(:bid).try(:inspection)
+        create_documents
         format.html { redirect_to @comm_history, notice: 'Bids follow up was successfully updated.' }
         format.json { render :show, status: :ok, location: @comm_history }
       else
@@ -107,6 +111,12 @@ class CommHistoriesController < ApplicationController
       @bids = Bid.where("status = ? OR status = ? ", "Pending", "Follow-up")
       @bids << @comm_history.try(:bid) if @comm_history and @comm_history.bid_id
       @bids = @bids.map{|b| [b.try(:title), b.id]}
+    end
+
+    def create_documents
+      params[:email_document_attributes].each do |file|
+        @inspection.documents << Document.new(attachment: file, document_type: "email" )
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

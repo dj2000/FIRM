1d66599633319e957e813982d00bb638c5bb2869
himesh <himesh@pixelcrayons.com>
2016-01-20 16:17:43 +0530

    added permit information link for project's form

--- app/models/project_payment_schedule.rb
+++ app/models/project_payment_schedule.rb
@@ -17,29 +17,31 @@ class ProjectPaymentSchedule < ActiveRecord::Base
 	end
 
 	def project_payment_schedules_invoice_date
-	  self.validation_payment_schedules.each do |index, attributes|
-			attributes["amount"] = attributes["amount"].to_f
-			attributes["paid"] = attributes["paid"] == "1" ? true : false
-			attributes["invoice_date"] = attributes["invoice_date"].present? ? attributes["invoice_date"].to_date : nil
-			attributes["date_paid"] = attributes["date_paid"].present? ? attributes["date_paid"].to_date : nil
-			attributes["payment_id"] = attributes["payment_id"].to_i
-			self_attributes = self.attributes
-			if self.new_record?
-				self_attributes =  self.attributes.except("id")
-			else
-				attributes[:id] = attributes[:id].to_i
-			end
-			if self_attributes.except("created_at", "updated_at", "project_id") == attributes.to_h
-	      if index != "0"
-	        prev_attribute = validation_payment_schedules[(index.to_i - 1).to_s]
-	        prev_invoice_date = prev_attribute["invoice_date"]
-	        if self.invoice_date.present? and prev_invoice_date.present?
-	          if self.invoice_date < prev_invoice_date.to_date
-	            self.errors.add(:invoice_date, "Invoice Date for #{self.payment_schedule} should not be greater than #{prev_attribute["payment_schedule"]}")
-	          end
-	        end
-	      end
-	    end
-	  end
+		if validation_payment_schedules.present?
+		  self.validation_payment_schedules.each do |index, attributes|
+				attributes["amount"] = attributes["amount"].to_f
+				attributes["paid"] = attributes["paid"] == "1" ? true : false
+				attributes["invoice_date"] = attributes["invoice_date"].present? ? attributes["invoice_date"].to_date : nil
+				attributes["date_paid"] = attributes["date_paid"].present? ? attributes["date_paid"].to_date : nil
+				attributes["payment_id"] = attributes["payment_id"].to_i
+				self_attributes = self.attributes
+				if self.new_record?
+					self_attributes =  self.attributes.except("id")
+				else
+					attributes[:id] = attributes[:id].to_i
+				end
+				if self_attributes.except("created_at", "updated_at", "project_id") == attributes.to_h
+		      if index != "0"
+		        prev_attribute = validation_payment_schedules[(index.to_i - 1).to_s]
+		        prev_invoice_date = prev_attribute["invoice_date"]
+		        if self.invoice_date.present? and prev_invoice_date.present?
+		          if self.invoice_date < prev_invoice_date.to_date
+		            self.errors.add(:invoice_date, "Invoice Date for #{self.payment_schedule} should not be greater than #{prev_attribute["payment_schedule"]}")
+		          end
+		        end
+		      end
+		    end
+		  end
+		end
   end
 end
